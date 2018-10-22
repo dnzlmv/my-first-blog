b713d8a377834a92b1eb85cfdb389150b431a309
Danny Wodrich <d.wodrich@outlook.com>
2018-10-22 15:03:00 +0200

    Final 1

--- blog/models.py
+++ blog/models.py
@@ -12,20 +12,20 @@ class Post(models.Model):
 	published_date = models.DateTimeField(
 		blank=True, null=True)
 
-	
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
-	
+
 	def __str__(self):
 		return self.title
-        
+
 	def approved_comments(self):
 		return self.comments.filter(approved_comment=True)
-        
-       
-        
-    
+
+
+
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
@@ -39,5 +39,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-    
