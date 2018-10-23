61a9fafe34e37242266a764650a1e7a463108a95
Danny Wodrich <d.wodrich@outlook.com>
2018-10-23 15:16:42 +0200

    Poll

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("Frage: %s." % question_id)
+
+def results(request, question_id):
+    response = "Antworten von Frage: %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Sie stimmen ab für Frage: %s." % question_id)
