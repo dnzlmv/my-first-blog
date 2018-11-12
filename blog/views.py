15b516bc0135ee343b94fe1fe8d9958e3426b24e
Danny Wodrich <d.wodrich@outlook.com>
2018-11-12 14:53:53 +0100

    Fix

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
             post.author = request.user
 
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog:post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
